10c1b87e4152364d81c58fca0c984b04e1d27e24
avayer2 <avayer.webdev@gmail.com>
2020-11-26 00:22:49 +0530

    working Custom

--- backend/url-model.js
+++ backend/url-model.js
@@ -2,8 +2,15 @@ const mongoose = require("mongoose");
 
 
 const urlSchema = mongoose.Schema({
-    longURL: {type: String, required: true},
-    customString: {type: String}
+  longURL: { type: String, required: true },
+  customUrl: {
+    type: String,
+    index: {
+      unique: true,
+      partialFilterExpression: { customUrl: { $type: "string" } }
+    },
+    default: null
+  }
 });
 
 module.exports = mongoose.model("URl", urlSchema);
